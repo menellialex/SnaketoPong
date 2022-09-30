820f928448be9ddd92390afaa826d5fbe9b707d9
Nick <nickmarsalek@yahoo.com>
2022-09-30 15:13:33 -0400

    Fixed typos and cleaned up code

--- Inc/circle_queue.h
+++ Inc/circle_queue.h
@@ -1,7 +1,8 @@
 //circle_queue.h
+//Made by Nick M
 
-#ifndef INC_CRCLE_QUEUE_STRUCT_H_
-#define INC_CIRCLE_QUEUE_STRUCT_H_
+#ifndef CIRCLE_QUEUE_H
+#define CIRCLE_QUEUE_H
 
 #include <stddef.h> // size_t
 #include <stdint.h>
@@ -14,29 +15,23 @@
 #define CIRCLE_Q_BUFSIZE 10
 
 typedef union {
-	//put our queue data in here
-		int int_val;
-		float float_val;
+    //put our queue data in here
+        int int_val;
+        float float_val;
 } Q_data;
 
 typedef struct circle_queue_struct {
-	// ATTRIBUTES
-	size_t cap;
-	size_t burden;
-	size_t tail;
-	size_t head;
-	Q_data buffer[CIRCLE_Q_BUFSIZE];
+    // ATTRIBUTES
+    size_t cap;
+    size_t burden;
+    size_t tail;
+    size_t head;
+    Q_data buffer[CIRCLE_Q_BUFSIZE];
 
-
-	//New Methods
-	
-	
-
-	
-	// METHODS
-	bool (*put)(struct circle_queue_struct *q, const Q_data *msg);
-	bool (*peek)(const struct circle_queue_struct *q, Q_data *msg);
-	bool (*get)(struct circle_queue_struct *q,  Q_data *msg);
+    // METHODS
+    bool (*put)(struct circle_queue_struct *q, const Q_data *msg);
+    bool (*peek)(const struct circle_queue_struct *q, Q_data *msg);
+    bool (*get)(struct circle_queue_struct *q,  Q_data *msg);
 } circle_queue;
 
 circle_queue* circle_queue_init( circle_queue* Q);
@@ -44,4 +39,4 @@ bool circle_queue_put(circle_queue *q, const Q_data *msg);
 bool circle_queue_peek(const circle_queue *q, Q_data *msg);
 bool circle_queue_get(circle_queue *q, Q_data *msg);
 
-#endif /* INC_CIRCLE_QUEUE_STRUCT_H_ */
+#endif /* CIRCLE_QUEUE_H */
--- Src/circle_queue.c
+++ Src/circle_queue.c
@@ -1,119 +1,94 @@
 //circle_queue.c
+//Made by Nick M
 #include "circle_queue.h"
+#include "stdio.h"
 
 
 circle_queue* circle_queue_init(circle_queue* q){
-	q->head = 0;
-	q->tail= 0;
-	q->cap = CIRCLE_Q_BUFSIZE;
-	q->burden = 0;
-	q->put = &(circle_queue_put);
-	q->get = &(circle_queue_get);
-	q->peek = &(circle_queue_peek);
-	return q;
+    q->head = -1;
+    q->tail= -1;
+    q->cap = CIRCLE_Q_BUFSIZE;
+    q->burden = 0;
+    q->put = &(circle_queue_put);
+    q->get = &(circle_queue_get);
+    q->peek = &(circle_queue_peek);
+    return q;
 }
 
 
-//bool circle_queue_put(circle_queue *q, const Q_data *msg){
-//	bool success = false;
-	// FIRST check if there is room in the queue
-//	if (q->burden >= q->cap) success = false;
-//	else {
-//		 q-> burden += 1;
-//		 q->buffer[q->tail] = *msg;
-//		 q->tail += 1;
-//		 success = true;
-//	}
-//	return success;
-//}
 
 bool circle_queue_put(circle_queue *q, const Q_data *msg){
-	bool success = false;
-	// FIRST check if there is room in the queue
-	if((q->head == 0) && (q->tail == q->cap-1) {
-		return success;
-	}
-	else if(q->head == -1)
-	{
-		q->head = q->tail;
-		q->buffer[q->tail] = *msg;
-		success = true;
-		return success;
-	}
-	
-	else if((q->tail == cap-1) &&(q->head != 0)
-	{
-		q->tail = 0;
-		q->buffer[q->tail] = *msg;
-		success = true;
-		return success;
-	}
-	else
-	{
-		q->tail += 1;
-		q->buffer[q->tail] = *msg;
-		success = true;
-		return success;
-	}
-	
-	return success;
+    bool success = false;
+    // FIRST check if there is room in the queue
+    if((q->head == 0) && (q->tail == q->cap-1)) {
+        return success;
+    }
+    else if(q->head == -1)
+    {
+        q->head = q->tail;
+        q->buffer[q->tail] = *msg;
+        success = true;
+        return success;
+    }
+
+    else if((q->tail == q->cap-1) && (q->head != 0))
+    {
+        q->tail = 0;
+        q->buffer[q->tail] = *msg;
+        success = true;
+        return success;
+    }
+    else
+    {
+        q->tail += 1;
+        q->buffer[q->tail] = *msg;
+        success = true;
+        return success;
+    }
+
+    return success;
 }
 
-//bool circle_queue_get(circle_queue *q, Q_data  *msg){
-//	bool success = false;
-	// FIRST check if there is data in the queue
-//	if (q->burden == 0) success= false;
-
-//	else {
-	    // Get message from front
-//		*msg = q->buffer[0];
-		// Shuffle others forward
-//		for (int n = 0; n < (q->tail - 1); n++){
-//		   q->buffer[n] = q->buffer[n+1];
-//		}
-		// Bookkeeping
-//		q-> tail -= 1;
-//		q->burden -= 1;
-//		success = true;
-//	}
-//	return success;
-//}
 
 bool circle_queue_get(circle_queue *q, Q_data  *msg){
-	bool success = false;
-	// FIRST check if there is data in the queue
-	if (q->head == -1)
+    bool success = false;
+    // FIRST check if there is data in the queue
+    if (q->head == -1)
     {
         return success;
     }
- 
+
     *msg = q->buffer[0];
-    q->buffer[q->head] = -1;
+    Q_data test;
+    test.int_val = -1;
+    q->buffer[q->head] = test;
     if (q->head == q->tail)
     {
         q->head = -1;
         q->tail = -1;
     }
-    else if (q->head == cap-1) {
+    else if (q->head == q->cap-1) {
         q->head = 0;
-	}
+    }
     else {
         q->head += 1;
-	}
-	success = true;
-	return success;
+    }
+    success = true;
+    return success;
 }
 
 
 bool circle_queue_peek(const circle_queue *q, Q_data  *msg){
-	bool success = false;
-	// FIRST check if there is data in the queue
-	if (q->head == -1) success = false;
-	// If YES - copy data but do not modify anything.
-	else {
-		*msg = q->buffer[0];
-		success = true;
-	}
-	return success;
+    bool success = false;
+    // FIRST check if there is data in the queue
+    if (q->head == -1){
+        success = false;
+    }
+    // If YES - copy data but do not modify anything.
+    else {
+        *msg = q->buffer[0];
+        success = true;
+    }
+    return success;
 }
 
