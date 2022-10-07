5c1bbd6145321b2d550a10200281787d3928af97
Alex Menelli <menellialex@gmail.com>
2022-10-07 15:09:14 -0400

    Pushed and merge into main
    includes Alex's screwed up files and a IOC explained file

---
+++ Inc/pong_gameplay.h
@@ -0,0 +1,64 @@
+/*
+ * snake_gameplay.h
+ *
+ *  Created on: Jul 28, 2022
+ *      Author: carrolls
+ */
+
+#ifndef SNAKE_GAMEPLAY_H_
+#define SNAKE_GAMEPLAY_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "quadknob.h"
+#include "pong_enums.h"
+#include "smc_queue.h"
+#include "pong_main.h" // Get checks and display width
+
+typedef struct {int16_t x; int16_t y;} XY_PT;
+
+typedef struct {
+	enum snake_compass_dir vertebra[CHECKS_WIDE * CHECKS_WIDE];
+	XY_PT head;
+	XY_PT fruit;
+	uint8_t length;
+	enum snake_compass_dir heading;
+} snake_game;
+
+enum snake_compass_dir snake_opposite_direction(enum snake_compass_dir d);
+void snake_game_init(snake_game* s);
+void snake_game_cleanup(snake_game* s);
+void snake_heading_update(snake_game* snake, Smc_queue* q);
+void snake_periodic_play(snake_game *snake);
+void snake_place_fruit(snake_game * s, const int8_t b[CHECKS_WIDE][CHECKS_WIDE]);
+bool snake_plot(const snake_game *s, int8_t b[CHECKS_WIDE][CHECKS_WIDE]);
+bool fruit_plot(const snake_game *s, int8_t b[CHECKS_WIDE][CHECKS_WIDE]);
+
+
+// start pong variables
+typedef struct
+{
+	XY_PT left_top;
+	XY_PT left_middle;
+	XY_PT left_bottom;
+	enum bar_direction left_direction;
+
+	XY_PT right_top;
+	XY_PT right_middle;
+	XY_PT right_bottom;
+	enum bar_direction right_direction;
+
+	XY_PT ball_position;
+	enum ball_direction ball_direction;
+} pong_game;
+
+//start pong gameplay functions
+void pong_init(pong_game* pg);
+void pong_plot(pong_game* pg, int8_t b[CHECKS_WIDE][CHECKS_WIDE]);
+void ball_plot(pong_game* pg, int8_t b[CHECKS_WIDE][CHECKS_WIDE]);
+void pong_periodic_play(pong_game*);
+void ball_state(pong_game* pg);
+void check_ball_collision(pong_game* pg);
+
+#endif /* SNAKE_GAMEPLAY_H_ */
+
---
+++ README.txt
@@ -0,0 +1,5 @@
+Initial Creation by Dr. Sean Carrol
+New updates done by Alex Menelli, Nick Marselek and Logan Young
+Trine University
+Embedded Systems Lab 
+ECE 371
