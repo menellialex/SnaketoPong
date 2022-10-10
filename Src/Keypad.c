43b784cb0dde875350aa403590f80256999bd950
layoung3 <94134691+layoung3@users.noreply.github.com>
2022-10-10 05:22:01 -0400

    added extra comments

--- Src/Keypad.c
+++ Src/Keypad.c
@@ -8,23 +8,23 @@
  #include "pressed.h"
  
  
-// player1_up player1_down player2_up player2_down empty
+// enum for reference  player1_up, player1_down, player2_up, player2_down, empty
  
-uint4_t scan_column(uint16_t pin_var){
+uint4_t scan_column(uint16_t pin_var){ //function to determine which culumn is being obstructed / pressed
 	uint4_t column;
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_SET); //Set columns high
 	
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
-	if (HAL_GPIO_WritePin(GPIOB, pin_var) == 0)
-		column = 1;
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET); //set column 1 high
+	if (HAL_GPIO_WritePin(GPIOB, pin_var) == 0) //checks if sent row is low
+		column = 1; //if it is sets int to 1
 	
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
-	if (HAL_GPIO_WritePin(GPIOB, pin_var) == 0)
-		column = 2;
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET); //reset column 1 low
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET); //set column 2 high
+	if (HAL_GPIO_WritePin(GPIOB, pin_var) == 0) //set column 2 high
+		column = 2; //if it is sets int to 1
 	
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_SET); //Set columns back low
-	return column;
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_SET); //Set both columns back low
+	return column; //return column that was pressed
 }
 
 
@@ -45,21 +45,21 @@ void keypad_init(Keypad *self) {
 	 HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET); //Set columns low
 	 
 	 if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0) == 0){
-		 column_num = scan_column(GPIO_PIN_0);
-		 if (column_num == 1)
+		 column_num = scan_column(GPIO_PIN_0); 
+		 if (column_num == 1) //if top row and the left column is pressed
 			 button_input = PLAYER1_UP;
-		 if (column_num == 2)
+		 if (column_num == 2) //if top row and right column is pressed
 			 button_input = PLAYER1_DOWN;
 	 }
 	 else if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1) == 0){
 		 column_num = scan_column(GPIO_PIN_0);
-		 if (column_num == 1)
+		 if (column_num == 1) //if bottom row and the left column is pressed
 			 button_input = PLAYER2_UP;
-		 if (column_num == 2)
+		 if (column_num == 2) //if bottom row and right column is pressed
 			 button_input = PLAYER2_DOWN;
 	 }
 	 else
-		 input_player1 = EMPTY;
+		 input_player1 = EMPTY; //if no pins are set low there is no input and return empty
 	 
 	 self->button_press = button_input;
 	 
